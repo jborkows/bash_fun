2fe219a76c15716c2985d2053bc73958aaf27c4b
Jacek Borkowski <jacek.borkowskiit@gmail.com>
2025-04-19 11:29:49 +0200

    github preprations

--- utils/display.sh
+++ utils/display.sh
@@ -1,20 +1,61 @@
 #!/usr/bin/env bash
-function title() {
-	echo "===================="
-	echo "$1"
-	echo "===================="
+function isCI() {
+	# Check if the script is running in a CI environment
+	if [[ "$CI" == "true" ]]; then
+		return 0 # true
+	else
+		return 1 # false
+	fi
 }
 
-function subtitle() {
-	echo "--------------------"
-	echo "$1"
-	echo "--------------------"
+function mainTitle() {
+
+	if isCI; then
+		echo "#"$1
+	else
+		echo "===================="
+		echo "  $1"
+		echo "===================="
+	fi
+}
+function title() {
+	if isCI; then
+		echo "##"$1
+	else
+		echo "===================="
+		echo "  $1"
+		echo "===================="
+	fi
+}
+
+function code() {
+	if isCI; then
+		echo '```bash'
+		cat "$1"
+		echo '```'
+	else
+		echo "--------------------"
+		echo "body"
+		echo "--------------------"
+		cat "$1"
+	fi
+}
+
+function effect() {
+	if isCI; then
+		echo '```'
+		bash "$1"
+		echo '```'
+	else
+		echo "--------------------"
+		echo "effect"
+		echo "--------------------"
+		cat "$1"
+	fi
 }
 
 function execution(){
 	title "$1"
-	subtitle body
-	cat "$2"
-	subtitle effect
-	bash "$2"
+	code $2
+	effect $2
 }
