f4e13d65b225c29d6a09ebd9d47b1e85de42e1b8
Jacek Borkowski <jacek.borkowskiit@gmail.com>
2025-04-19 11:54:35 +0200

    typo

--- lsfun/summary.sh
+++ lsfun/summary.sh
@@ -2,15 +2,15 @@
 source ../utils/display.sh
 
 mainTitle "Playing with ls"
-title "ls"
+title "ls -l"
 if isCI; then
-	echo '```bash'
-	ls ./files
+	echo '```'
+	ls -l ./files
 	echo '```'
 else
 	ls ./files
 fi
 
 execution "script ls into for loop (uses spaces as separator)" first.sh
-execution "script ls into for loop with globbing" second.sh
+execution "script using for in with globbing" second.sh
 
